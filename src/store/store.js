3a893717adfbe966628c2983257866c2d39ea653
ashutoshgiri <ashutoshgiri249@gmail.com>
2022-11-27 12:43:54 +0530

    added store(redux) updated reducers in the context

---
+++ src/store/store.js
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import { userReducer } from './user/user.reducer'
+import { compose, applyMiddleware} from 'redux'
+
+import logger from 'redux-logger'
+
+const middleWares = [logger]
+const composedEnhancers = compose(applyMiddleware(...middleWares))
+
+const reducer = {
+    user: userReducer,
+}
+
+export const store = configureStore({
+    reducer,
+    undefined,
+    composedEnhancers
+})
